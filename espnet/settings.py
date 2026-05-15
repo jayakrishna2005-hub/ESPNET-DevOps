cd70cea780f24e212fc7b6065733cb520a6b9683
jayakrishna <jayakrishnajk2005@gmail.com>
2026-05-15 21:56:00 +0530

    Add GitHub Actions CI pipeline

--- espnet/settings.py
+++ espnet/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'espnet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'espnet_db',
+        'USER': 'espnet_user',
+        'PASSWORD': 'espnet_password',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
